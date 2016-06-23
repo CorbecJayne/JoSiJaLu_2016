eca55768b14716aa14ec03cafbdb085e0ac74191
unknown <lulalein99@web.de>
2016-06-23 14:34:53 +0200

    Änderungen - Bewegung in diagonale Richtungen wird nun extra geprüft und ist halbiert - Das drücken zweier entgegengesetzter Tasten führt zum Stillstand des Spielers - Zweiter Spieler erstellt          - kann sich wie der erste bewegen          - benutzt dabei die Richtungstasten
    Der ganze Code ist nicht der schönste aber er funktioniert (Wenn irgendwas nicht stimmt unbedingt melden und ein Issue erstellen)
    Es lässt sich bestimmt einiges verbessern

--- Code/desktop/src/com/josijalu/game/JoSiJaLu.java
+++ Code/desktop/src/com/josijalu/game/JoSiJaLu.java
@@ -17,8 +17,9 @@ public class JoSiJaLu implements Screen {
     private OrthographicCamera camera;
     private Texture projectile_graphic;
     public SpriteBatch batch;
-    private Player player;
-    private ArrayList<Player> players = new ArrayList<Player>();
+    private Player player1;
+    private Player player2;
+//    private ArrayList<Player> players = new ArrayList<Player>(); Unused
     private ArrayList<Projectile> bullets = new ArrayList<Projectile>();
 
     //creating objects
@@ -30,7 +31,8 @@ public class JoSiJaLu implements Screen {
         //this is important for the grafical stuff
         game.batch = new SpriteBatch();
         //creating the player incl. graphic
-        player = new Player("bird", new Vector2(1856, 0));
+        player1 = new Player("bird", new Vector2(0, 0));
+        player2 = new Player("pig", new Vector2(1856, 0));
         projectile_graphic = new Texture(Gdx.files.internal("graphics/projectile.png"));
     }
 
@@ -51,31 +53,137 @@ public class JoSiJaLu implements Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         //update the camera view (camera is fullscreen so until now there is nothing to update)
         camera.update();
-        //makes the player move with the speed that is saved in his class
-        int speed = player.getSpeed();
-        float x = player.getPosition().x;
-        float y = player.getPosition().y;
-        if (Gdx.input.isKeyPressed(Input.Keys.A)) x -= speed * 100 * Gdx.graphics.getDeltaTime();
-        if (Gdx.input.isKeyPressed(Input.Keys.D)) x += speed * 100 * Gdx.graphics.getDeltaTime();
-        if (Gdx.input.isKeyPressed(Input.Keys.S)) y -= speed * 100 * Gdx.graphics.getDeltaTime();
-        if (Gdx.input.isKeyPressed(Input.Keys.W)) y += speed * 100 * Gdx.graphics.getDeltaTime();
-        if (x < 0) x = 0;
-        if (x > game.width - player.getSize()) x = game.width - player.getSize();
-        if (y < 0) y = 0;
-        if (y > game.height - player.getSize()) y = game.height - player.getSize();
-        player.setPosition(x, y);
+
+        //Movement Player 1
+        //makes the player 1 move with the speed that is saved in his class
+        int speed1 = player1.getSpeed()*100;
+        float x1 = player1.getPosition().x;
+        float y1 = player1.getPosition().y;
+        //When holding down a Button, the Player will move in one vertical or horizontal direction
+        if (Gdx.input.isKeyPressed(Input.Keys.A) &&! Gdx.input.isKeyPressed(Input.Keys.W) &&! Gdx.input.isKeyPressed(Input.Keys.S) &&! Gdx.input.isKeyPressed(Input.Keys.D)) {
+            x1 -= speed1 * Gdx.graphics.getDeltaTime();
+        }
+
+        else if (Gdx.input.isKeyPressed(Input.Keys.D) &&! Gdx.input.isKeyPressed(Input.Keys.W) &&! Gdx.input.isKeyPressed(Input.Keys.S) &&! Gdx.input.isKeyPressed(Input.Keys.A)) {
+            x1 += speed1 * Gdx.graphics.getDeltaTime();
+        }
+
+        else if (Gdx.input.isKeyPressed(Input.Keys.S) &&! Gdx.input.isKeyPressed(Input.Keys.W) &&! Gdx.input.isKeyPressed(Input.Keys.A) &&! Gdx.input.isKeyPressed(Input.Keys.D)){
+            y1 -= speed1 * Gdx.graphics.getDeltaTime();
+        }
+
+        else if (Gdx.input.isKeyPressed(Input.Keys.W) &&! Gdx.input.isKeyPressed(Input.Keys.A) &&! Gdx.input.isKeyPressed(Input.Keys.S) &&! Gdx.input.isKeyPressed(Input.Keys.D)){
+            y1 += speed1 * Gdx.graphics.getDeltaTime();
+        }
+        //When holding down two Buttons at the same time, the Player will move in one diagonal direction
+        else if (Gdx.input.isKeyPressed(Input.Keys.A) && Gdx.input.isKeyPressed(Input.Keys.S)) {
+            x1 -= (speed1 * Gdx.graphics.getDeltaTime()) / 2;
+            y1 -= (speed1 * Gdx.graphics.getDeltaTime()) / 2;
+        }
+        else if (Gdx.input.isKeyPressed(Input.Keys.D) && Gdx.input.isKeyPressed(Input.Keys.W)) {
+            x1 += (speed1 * Gdx.graphics.getDeltaTime()) / 2;
+            y1 += (speed1 * Gdx.graphics.getDeltaTime()) / 2;
+        }
+
+        else if (Gdx.input.isKeyPressed(Input.Keys.S) && Gdx.input.isKeyPressed(Input.Keys.D)) {
+
+            y1 -= (speed1 * Gdx.graphics.getDeltaTime()) / 2;
+            x1 += (speed1 * Gdx.graphics.getDeltaTime()) / 2;
+        }
+
+        else if (Gdx.input.isKeyPressed(Input.Keys.W) && Gdx.input.isKeyPressed(Input.Keys.A)) {
+            x1 -= (speed1 * Gdx.graphics.getDeltaTime())/ 2;
+            y1 += (speed1 * Gdx.graphics.getDeltaTime())/ 2;
+        }
+        //When holding down two Buttons at the same time, the Player will stop moving
+        else if (Gdx.input.isKeyPressed(Input.Keys.W) && Gdx.input.isKeyPressed(Input.Keys.S)) {
+
+        }
+
+        else if (Gdx.input.isKeyPressed(Input.Keys.A) && Gdx.input.isKeyPressed(Input.Keys.D)) {
+
+        }
+
+
+        //Movement Player 2
+        //makes the player 2 move with the speed that is saved in his class
+        int speed2 = player2.getSpeed()*100;
+        float x2 = player2.getPosition().x;
+        float y2 = player2.getPosition().y;
+        //When holding down a Button, the Player will move in one vertical or horizontal direction
+        if (Gdx.input.isKeyPressed(Input.Keys.DPAD_LEFT) &&! Gdx.input.isKeyPressed(Input.Keys.DPAD_UP) &&! Gdx.input.isKeyPressed(Input.Keys.DPAD_DOWN) &&! Gdx.input.isKeyPressed(Input.Keys.DPAD_RIGHT)) {
+            x2 -= speed2 * Gdx.graphics.getDeltaTime();
+        }
+
+        else if (Gdx.input.isKeyPressed(Input.Keys.DPAD_RIGHT) &&! Gdx.input.isKeyPressed(Input.Keys.DPAD_UP) &&! Gdx.input.isKeyPressed(Input.Keys.DPAD_DOWN) &&! Gdx.input.isKeyPressed(Input.Keys.DPAD_LEFT)) {
+            x2 += speed2 * Gdx.graphics.getDeltaTime();
+        }
+
+        else if (Gdx.input.isKeyPressed(Input.Keys.DPAD_DOWN) &&! Gdx.input.isKeyPressed(Input.Keys.DPAD_UP) &&! Gdx.input.isKeyPressed(Input.Keys.DPAD_LEFT) &&! Gdx.input.isKeyPressed(Input.Keys.DPAD_RIGHT)){
+            y2 -= speed2 * Gdx.graphics.getDeltaTime();
+        }
+
+        else if (Gdx.input.isKeyPressed(Input.Keys.DPAD_UP) &&! Gdx.input.isKeyPressed(Input.Keys.DPAD_LEFT) &&! Gdx.input.isKeyPressed(Input.Keys.DPAD_DOWN) &&! Gdx.input.isKeyPressed(Input.Keys.DPAD_RIGHT)){
+            y2 += speed2 * Gdx.graphics.getDeltaTime();
+        }
+        //When holding down two Buttons at the same time, the Player will move in one diagonal direction
+        else if (Gdx.input.isKeyPressed(Input.Keys.DPAD_LEFT) && Gdx.input.isKeyPressed(Input.Keys.DPAD_DOWN)) {
+            x2 -= (speed2 * Gdx.graphics.getDeltaTime()) / 2;
+            y2 -= (speed2 * Gdx.graphics.getDeltaTime()) / 2;
+        }
+        else if (Gdx.input.isKeyPressed(Input.Keys.DPAD_RIGHT) && Gdx.input.isKeyPressed(Input.Keys.DPAD_UP)) {
+            x2 += (speed2 * Gdx.graphics.getDeltaTime()) / 2;
+            y2 += (speed2 * Gdx.graphics.getDeltaTime()) / 2;
+        }
+
+        else if (Gdx.input.isKeyPressed(Input.Keys.DPAD_DOWN) && Gdx.input.isKeyPressed(Input.Keys.DPAD_RIGHT)) {
+
+            y2 -= (speed2 * Gdx.graphics.getDeltaTime()) / 2;
+            x2 += (speed2 * Gdx.graphics.getDeltaTime()) / 2;
+        }
+
+        else if (Gdx.input.isKeyPressed(Input.Keys.DPAD_UP) && Gdx.input.isKeyPressed(Input.Keys.DPAD_LEFT)) {
+            x2 -= (speed2 * Gdx.graphics.getDeltaTime())/ 2;
+            y2 += (speed2 * Gdx.graphics.getDeltaTime())/ 2;
+        }
+        //When holding down two Buttons at the same time, the Player will stop moving
+        else if (Gdx.input.isKeyPressed(Input.Keys.DPAD_UP) && Gdx.input.isKeyPressed(Input.Keys.DPAD_DOWN)) {
+
+        }
+
+        else if (Gdx.input.isKeyPressed(Input.Keys.DPAD_LEFT) && Gdx.input.isKeyPressed(Input.Keys.DPAD_RIGHT)) {
+
+        }
+
+
+        //Prove if players colliding with window border
+        if (x1 < 0) x1 = 0;
+        if (x1 > game.width - player1.getSize()) x1 = game.width - player1.getSize();
+        if (y1 < 0) y1 = 0;
+        if (y1 > game.height - player1.getSize()) y1 = game.height - player1.getSize();
+
+
+        if (x2 < 0) x2 = 0;
+        if (x2 > game.width - player2.getSize()) x2 = game.width - player2.getSize();
+        if (y2 < 0) y2 = 0;
+        if (y2 > game.height - player2.getSize()) y2 = game.height - player2.getSize();
+
+        player1.setPosition(x1, y1);
+        player2.setPosition(x2, y2);
+
         //update the mouse position in the Playerclass
-        player.update_mouse_position();
+        player1.update_mouse_position();
         //handle the shooting
         if (Gdx.input.isButtonPressed(Input.Keys.LEFT) && bullets.isEmpty()) {
-            bullets.add(new Projectile(player.getPosition(), player.getDirectionVector(), player));
+            bullets.add(new Projectile(player1.getPosition(), player1.getDirectionVector(), player1));
         }
         //camera stuff combined with drawing stuff
         game.batch.setProjectionMatrix(camera.combined);
         //drawing the objects
         game.batch.begin();
-        game.batch.draw(player.getGraphic(), player.getPosition().x, player.getPosition().y, player.getSize(), player.getSize());
-        game.batch.draw(player.getGraphicAim(), player.getMouse_position().x, player.getMouse_position().y, 64, 64);
+        game.batch.draw(player1.getGraphic(), player1.getPosition().x, player1.getPosition().y, player1.getSize(), player1.getSize());
+        game.batch.draw(player2.getGraphic(), player2.getPosition().x, player2.getPosition().y, player2.getSize(), player2.getSize());
+        game.batch.draw(player1.getGraphicAim(), player1.getMouse_position().x, player1.getMouse_position().y, 64, 64);
         //showing the Bullets and removing them if they traveled to far
         Iterator<Projectile> bulletsI = bullets.iterator();
         while (bulletsI.hasNext()) {
@@ -113,8 +221,9 @@ public class JoSiJaLu implements Screen {
     //cleanup if closing the game screen
     @Override
     public void dispose() {
-        player.getGraphic().dispose();
-        player.getGraphicAim().dispose();
+        player1.getGraphic().dispose();
+        player2.getGraphic().dispose();
+        player1.getGraphicAim().dispose();
         batch.dispose();
         projectile_graphic.dispose();
     }
